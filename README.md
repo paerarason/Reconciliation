4d4917f3722bb67a96fc537709c1917cc6aec143
your_github_username <paerarason1111@gmail.com>
2023-07-07 19:00:55 +0530

    SEPARATE QUERY

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Reconciliation
+
+
+## CREATE ORDER
+```sh
+/order
+````
---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/paerarason/Reconciliation/controller"
+)
+
+func main() {
+
+	r := gin.Default()
+	r.POST("/order", controller.OrderCreate)
+	r.POST("//identify", controller.OrderCreate)
+
+}
